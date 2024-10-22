5cc630e8515d147685cf08db0cfc08ebbd972ed7
CarnifX <thecarnifx@gmail.com>
2024-10-22 16:28:46 +0200

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -5,15 +5,14 @@ public class Main {
 
     public static void main(String[] args) throws SQLException {
 
-        String envVar = System.getenv("SERVER_PASSWORD");
-        System.out.println(envVar);
-
+        //Lager en connection instance som snakker med den lokale SQL serveren.
         Connection connection = null;
         try {
 
             connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/sql_memberserver", "root", "");
             Statement statement = connection.createStatement();
 
+            //Kaller inn metoden for å opprette et nytt medlem. Sender med connection statementen.
             createNewMember.newMember(statement);
 
 
