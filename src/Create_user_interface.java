fb698d21a5f46514c997ead2bf3dd113c79b5946
CarnifX <thecarnifx@gmail.com>
2024-10-23 18:52:04 +0200

    Initial commit

---
+++ src/Create_user_interface.java
@@ -0,0 +1,57 @@
+import javax.swing.*;
+
+public class Create_user_interface {
+
+    public static void create_user_interface(){
+
+        JFrame frame = new JFrame("Login");
+        JPanel panel = new JPanel();
+
+        frame.setSize(340, 200);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.add(panel);
+        panel.setLayout(null);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+
+        JLabel username = new JLabel("Username: ");
+        username.setBounds(30, 20, 100, 25);
+        panel.add(username);
+
+        JTextField userText = new JTextField();
+        userText.setBounds(120, 20, 165, 25);
+        panel.add(userText);
+
+        JLabel password = new JLabel("Password: ");
+        password.setBounds(30, 50, 100, 25);
+        panel.add(password);
+
+        JPasswordField passwordText = new JPasswordField();
+        passwordText.setBounds(120, 50, 165, 25);
+        panel.add(passwordText);
+
+        JButton acceptButton = new JButton("Accept");
+        acceptButton.setBounds(210, 100, 80, 25);
+        panel.add(acceptButton);
+
+        JButton signUpButton = new JButton("Sign up");
+        signUpButton.setBounds(120, 100, 80, 25);
+        panel.add(signUpButton);
+
+        JButton cancelButton = new JButton("Cancel");
+        cancelButton.setBounds(30, 100, 80, 25);
+        panel.add(cancelButton);
+
+        frame.getRootPane().setDefaultButton(acceptButton);
+
+        acceptButton.addActionListener(e -> {
+            String written_username = userText.getText();
+            String written_password = String.valueOf(passwordText.getPassword());
+        });
+        signUpButton.addActionListener(ActionEvent -> Create_new_member.newMember());
+        cancelButton.addActionListener(ActionEvent -> frame.dispose());
+
+
+    }
+
+}
